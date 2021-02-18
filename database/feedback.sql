b9f17788cacf44fb8cb59ad513dceae18dc46d8d
Isoyan Inna <isoyan.inn@gmail.com>
2021-02-18 18:43:35 +0400

    added error handler, add feedback function, editing delete action

--- database/feedback.sql
+++ database/feedback.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Feb 16, 2021 at 02:38 PM
+-- Generation Time: Feb 18, 2021 at 02:42 PM
 -- Server version: 5.7.24
 -- PHP Version: 7.4.1
 
@@ -34,7 +34,7 @@ CREATE TABLE `messages` (
   `lastname` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
   `message` text NOT NULL,
-  `sent_at` date NOT NULL
+  `sent_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -42,7 +42,13 @@ CREATE TABLE `messages` (
 --
 
 INSERT INTO `messages` (`id`, `firstname`, `lastname`, `email`, `message`, `sent_at`) VALUES
-(1, 'anun', 'azganun', 'anunazganun@gmail.com', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis molestie urna sollicitudin lectus efficitur, nec viverra sapien euismod. In imperdiet diam nec volutpat mollis. Sed id lectus felis. Donec rhoncus scelerisque ligula eu pulvinar. In dignissim pellentesque sapien vitae egestas. Phasellus nisl dui, sollicitudin nec mattis in, eleifend sit amet enim. Proin quis augue vitae felis molestie viverra. Fusce vel leo sed nunc elementum maximus. Praesent semper laoreet felis, ac varius sapien pellentesque sit amet. Nulla in lectus aliquet, interdum urna id, elementum elit. Mauris cursus quis mi non volutpat. Sed vitae augue enim. Nam est dui, ultricies in efficitur in, tempor ac lectus. Cras neque leo, tincidunt et nibh eget, bibendum euismod felis. Fusce lacinia quis ipsum sed varius. Aenean eu dignissim velit.\r\n\r\nUt sodales sagittis lobortis. Fusce nec elit sed sapien efficitur varius. Cras et ultrices libero. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc mollis ligula metus, in sollicitudin justo elementum vel. Donec sagittis tortor a gravida dignissim. Etiam quis dui non orci pharetra ultricies tristique id augue. Donec sit amet commodo turpis. Aenean ex lorem, ornare a fringilla ut, commodo ac ante. Duis ut sem enim.', '2021-02-16');
+(1, 'anun', 'azganun', 'anunazganun@gmail.com', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis molestie urna sollicitudin lectus efficitur, nec viverra sapien euismod. In imperdiet diam nec volutpat mollis. Sed id lectus felis. Donec rhoncus scelerisque ligula eu pulvinar. In dignissim pellentesque sapien vitae egestas. Phasellus nisl dui, sollicitudin nec mattis in, eleifend sit amet enim. Proin quis augue vitae felis molestie viverra. Fusce vel leo sed nunc elementum maximus. Praesent semper laoreet felis, ac varius sapien pellentesque sit amet. Nulla in lectus aliquet, interdum urna id, elementum elit. Mauris cursus quis mi non volutpat. Sed vitae augue enim. Nam est dui, ultricies in efficitur in, tempor ac lectus. Cras neque leo, tincidunt et nibh eget, bibendum euismod felis. Fusce lacinia quis ipsum sed varius. Aenean eu dignissim velit.\r\n\r\nUt sodales sagittis lobortis. Fusce nec elit sed sapien efficitur varius. Cras et ultrices libero. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc mollis ligula metus, in sollicitudin justo elementum vel. Donec sagittis tortor a gravida dignissim. Etiam quis dui non orci pharetra ultricies tristique id augue. Donec sit amet commodo turpis. Aenean ex lorem, ornare a fringilla ut, commodo ac ante. Duis ut sem enim.', '2021-02-16 00:00:00'),
+(2, 'Inna', 'Isoyan', 'isoyan.inn@gmail.com', 'Feedback', '2021-02-18 16:59:58'),
+(3, 'Inna', 'Isoyan', 'isoyan.inn@gmail.com', 'Feedback', '2021-02-18 17:00:17'),
+(4, 'Unknown', 'unknown', 'unknown@mail.com', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', '2021-02-18 17:11:20'),
+(5, 'Fname', 'Lname', 'FLname@gmail.com', 'ed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est, qui do lorem ipsum, quia dolor sit amet consectetur adipisci[ng] velit, sed quia non numquam [do] eius modi tempora inci[di]dunt, ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum[d] exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? [D]Quis autem vel eum iure reprehenderit, qui in ea voluptate velit esse, quam nihil molestiae consequatur, vel illum, qui dolorem eum fugiat, quo voluptas nulla pariatur? [33] At vero eos et accusamus et iusto odio dignissimos ducimus, qui blanditiis praesentium voluptatum deleniti atque corrupti, quos dolores et quas molestias excepturi sint, obcaecati cupiditate non provident, similique sunt in culpa, qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta ', '2021-02-18 17:15:25'),
+(6, 'Inna', 'Isoyan', 'isoyan.inn@gmail.com', 'DELETE FROM `messages` WHERE 0DELETE FROM `messages` WHERE 0DELETE FROM `messages` WHERE 0DELETE FROM `messages` WHERE 0DELETE FROM `messages` WHERE 0', '2021-02-18 17:33:22'),
+(7, 'Fname', 'fdjs', 'dslkklfd@jdfk', 'ska,sff', '2021-02-18 17:45:53');
 
 -- --------------------------------------------------------
 
@@ -80,7 +86,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `messages`
 --
 ALTER TABLE `messages`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `users`
